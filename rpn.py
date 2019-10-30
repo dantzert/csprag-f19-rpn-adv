6d07f463ba478604a9d65d541af9245ef88733f8
Tiny Dantzer <dantzert@umich.edu>
2019-10-30 18:17:48 -0400

    import rpn.py from lecture
    this one uses an operator hashtable and the operator dictionary instead of a
    bunch of conditionals

--- rpn.py
+++ rpn.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
-# balh balh blah
+
+import operator
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'^': operator.pow,
+	'/': operator.floordiv,
+}
+
 def calculate(string):
 	stack = list()
 	for token in string.split():
-		if token == '+':
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError: # this must be an operator
+			try: 
+				
+				function = operator.__dict__[token]
+
+			except KeyError:
+				function = operators[token]
 			arg2 = stack.pop()
 			arg1 = stack.pop()
-			result = arg1 + arg2
+			result = function(arg1,arg2)
 			stack.append(result)
-		elif token == '-':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 - arg2
-			stack.append(result)
-		elif token == '^':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1**arg2
-			stack.append(result)
-		elif token == 'Q':
-			# this code won't get tested
-			stack.pop()
-			stack.pop()
-			print("what have you done")
-		else:
-			stack.append(int(token))
-	
-		print(stack)
-	
+		
 	if len(stack) != 1:
 		raise TypeError("malformed input: "+ string)
-
+	print(stack[0])
 	return stack.pop()
 
 def main():
