c54cc690d8e1aeb6165caad9bde44c228056f940
Tiny Dantzer <dantzert@umich.edu>
2019-10-30 18:29:14 -0400

    add support for singletons via math module

--- rpn.py
+++ rpn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import operator
+import math # support singleton operations
 
 operators = {
 	'+': operator.add,
@@ -17,17 +18,33 @@ def calculate(string):
 			value = int(token)
 			stack.append(value)
 		except ValueError: # this must be an operator
-			try: 
-				
-				function = operator.__dict__[token]
 
-			except KeyError:
-				function = operators[token]
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = function(arg1,arg2)
-			stack.append(result)
-		
+			if (len(stack) == 2):
+
+				try: 
+				
+					function = operator.__dict__[token]
+				except KeyError:
+			
+					function = operators[token]
+			
+			
+			
+				arg2 = stack.pop()
+				arg1 = stack.pop()
+				result = function(arg1,arg2)
+				stack.append(result)
+			elif (len(stack) == 1):
+				try:
+					function = math.__dict__[token]
+
+				except KeyError:
+
+					raise TypeError("no singleton function found for: "+ string)
+
+				arg = stack.pop()
+				result = function(arg)
+				stack.append(result)
 	if len(stack) != 1:
 		raise TypeError("malformed input: "+ string)
 	print(stack[0])
